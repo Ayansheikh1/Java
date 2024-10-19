4338774257c486ff613eacac81ceefc6c3747668
Ayansheikh1 <ayansheikh0698@gmail.com>
2024-10-19 22:08:06 +0530

    learn primitive and reference

--- Java-resourse/src/Main.java
+++ Java-resourse/src/Main.java
@@ -1,7 +1,14 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        int age = 21;
+        int age = 21;//primitive type
         System.out.println("Hello world!");
         System.out.println(age);
+
+
+        Date now =  new Date();//reference type
+        System.out.println(now);
+
     }
 }
