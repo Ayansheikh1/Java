06a5bcfc2264f7a4701102e2ca0239fb7d86140d
Ayansheikh1 <ayansheikh0698@gmail.com>
2024-10-28 22:23:03 +0530

    code some maths

---
+++ Java-resourse/src/AreaAndPerimeter.java
@@ -0,0 +1,12 @@
+public class AreaAndPerimeter {
+
+    public static void main(String[] args) {
+        double radius = 7.5;
+        double area = Math.PI * radius * radius;
+        double perimeter = 2 * Math.PI * radius;
+
+        System.out.println("area of circle is "+ area);
+        System.out.println("perimeter of circle is "+ perimeter);
+    }
+
+}
